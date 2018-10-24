f403d5a3a41ce54dfdc5791b5c23840f02c71db3
Mirwais Nezami <mirwais79@hotmail.com>
2018-10-24 09:18:25 +0200

    Footer mail-to link fix

--- Breakout Online/www/JS/frontCardsTranslation.js
+++ Breakout Online/www/JS/frontCardsTranslation.js
@@ -1,28 +1,3 @@
-/*
-$.getJSON('/json/frontCarsTranslation.json', translate); //calling json
-let jsonData;
-let languageIsSv = true;
-
-function translate (frontCards) {
- jsonData = frontCards;
- let lang = languageIsSv ? "sv" : "en";
-
- //$('.card-body').text(frontCards.cardText[lang]);
-
- $('.card-body').empty();
-     for( let text of frontCards.text[lang]){
-         let pTag = '<p>'+text+'</p>';
-         $('.card-body').append(pTag);
-     }
-  }
-
-  $('.flag').click(function(){
-      $('.flagDesign flagPlacement flag britFlag').toggle();
-      $('.flagDesign flagPlacement flag sweFlag').toggle();
-      languageIsSv= !languageIsSv;
-      translate(jsonData);
-  });
-*/
 
 /*
 
