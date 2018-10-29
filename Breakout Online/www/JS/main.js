15c7a9db230c950d5b17183119f3715c0e576516
Pontus Nilsson <p0nta_@hotmail.com>
2018-10-29 16:28:59 +0100

    spara som commit.

--- Breakout Online/www/JS/main.js
+++ Breakout Online/www/JS/main.js
@@ -1,3 +1,14 @@
+
+$(".testclass").on("click", function(e){
+    $("a").removeClass("active");
+    $(this).addClass("active");
+  });
+
+$(".testclass").click(function () {
+    $("a").removeClass( "active" );
+    $( ".testclass" ).last().addClass( "active" );
+})
+
 //used to position the text when printing
 let index = 1;
 //used to set the language between swedish and english
@@ -86,7 +97,7 @@ $(document).ready(function() {
         const a = document.createElement('a');
 
         a.setAttribute('href', href);
-        a.setAttribute('class', 'NavSweLang');
+        a.setAttribute('class', 'NavSweLang testclass');
         a.innerHTML = text;
 
         return a;
@@ -152,21 +163,3 @@ $(document).ready(function() {
     initHeader();
 });
 
-
-/*
-//hides the swedish headliner and shows the english headliner
-//on the start page when the british flag is clicked
-$('.britFlag').click(function() {
-    $('.textClassSwe').hide()
-    $('.textClassEng').show()
-})
-
-//hides the english headliner and shows the swedish headliner
-//on the start page when the swedish flag is clicked
-$('.sweFlag').click(function () {
-    $('.textClassEng').hide()
-    $('.textClassSwe').show()
-    $('.textClassEng').hide()
-    
-})
-*/
